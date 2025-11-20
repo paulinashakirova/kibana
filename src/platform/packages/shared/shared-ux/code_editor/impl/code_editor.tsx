6e8630779a38e324d79ce06781b47622984d3adf
paulina.shakirova <paulina.shakirova@elastic.co>
2025-11-20 09:54:46 +0100

    revert editor option colorization

--- src/platform/packages/shared/shared-ux/code_editor/impl/code_editor.tsx
+++ src/platform/packages/shared/shared-ux/code_editor/impl/code_editor.tsx
@@ -666,9 +666,8 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
                 lineHeight: isFullScreen ? 24 : 21,
                 contextmenu: enableCustomContextMenu,
                 fixedOverflowWidgets: true,
-                bracketPairColorization: {
-                  enabled: false,
-                },
+                // @ts-expect-error, see https://github.com/microsoft/monaco-editor/issues/3829
+                'bracketPairColorization.enabled': false,
                 ...options,
                 // Explicit links prop always takes precedence over any value passed in options
                 links,
